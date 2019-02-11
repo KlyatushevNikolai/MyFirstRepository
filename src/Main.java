d2f6091e7516a796917156983238a0f7793ba2e7
Николай <Nikolak12345@outlook.com>
2019-02-11 16:41:52 +0300

    3 строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello, is java file!!!");
+        System.out.println("Hello, is java fil1!!!");
+        System.out.println("Hello, is java file2!!!");
     }
 }
